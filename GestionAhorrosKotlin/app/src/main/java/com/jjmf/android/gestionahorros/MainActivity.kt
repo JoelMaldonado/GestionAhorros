d9c3ac0f5389ee575cfff9ea2e8a79f2e0768c26
JoelMaldonado <106793712+JoelMaldonado@users.noreply.github.com>
2024-02-21 15:49:38 -0500

    nuevos proyectos

---
+++ GestionAhorrosKotlin/app/src/main/java/com/jjmf/android/gestionahorros/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.jjmf.android.gestionahorros
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jjmf.android.gestionahorros.ui.theme.GestionAhorrosKotlinTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GestionAhorrosKotlinTheme {
+               
+                Text(text = "Holffdffa")
+            }
+        }
+    }
+}
---
+++ GestionAhorrosSwift/GestionAhorrosSwift/GestionAhorrosSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestionAhorrosSwiftApp.swift
+//  GestionAhorrosSwift
+//
+//  Created by Joel on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GestionAhorrosSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
